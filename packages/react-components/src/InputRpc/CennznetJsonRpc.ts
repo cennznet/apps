5f64c628c594c1d0ac7890aebea81b3b8ebdbc2f
karishma <bothara.karishma@gmail.com>
2020-07-06 13:05:11 +1200

    Added changes required to support RPC call

---
+++ packages/react-components/src/InputRpc/CennznetJsonRpc.ts
@@ -0,0 +1,40 @@
+import defaultJsonRpc from '@polkadot/jsonrpc';
+import createMethod from '@polkadot/jsonrpc/create/method';
+import createParam from '@polkadot/jsonrpc/create/param';
+import  userBare  from '@cennznet/api/rpc';
+
+const userRpc = Object.entries(userBare).reduce((user, [sectionName, methods]) => {
+    // @ts-ignore
+    user[sectionName] =  { methods: methods.reduce((section, def) => {
+        const {
+            description = 'User defined',
+            name,
+            params,
+            type
+        } = def;
+
+            section[name] = createMethod(sectionName, name, {
+            description,
+            params: params.map(({
+                                    // @ts-ignore
+                                    isOptional,
+                                    // @ts-ignore
+                                    name,
+                                    // @ts-ignore
+                                    type
+                                }) => createParam(name, type, {
+                isOptional
+            })),
+            type: type
+        });
+        return section;
+    }, {})
+    }
+    return user;
+}, {}); // decorate the sections with base and user methods
+
+const cennznetJsonRpc = Object.assign({}, defaultJsonRpc,
+    userRpc
+);
+
+export default cennznetJsonRpc;
