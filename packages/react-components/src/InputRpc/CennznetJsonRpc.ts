0e8ee73775b929cb27d0639c5bb00fd13fa6537d
KarishmaBothara <bothara.karishma@gmail.com>
2020-12-04 16:53:43 +1300

    as per review comment

--- packages/react-components/src/InputRpc/CennznetJsonRpc.ts
+++ packages/react-components/src/InputRpc/CennznetJsonRpc.ts
@@ -4,7 +4,8 @@ import {DefinitionRpcExt} from '@polkadot/types/types';
 import * as definitions from '@cennznet/types/interfaces/definitions';
 
 const cennznetRpc: Record<string, Record<string, DefinitionRpcExt>> = {};
-
+// Have constructed cennznetRpc object in the same way as https://github.com/polkadot-js/api/blob/master/packages/types/src/interfaces/jsonrpc.ts
+// 'cennzx' is used to let typescript know how section would look like, in upstream 'babe' is used for the same reason
 Object
     .keys(definitions)
     .filter((key: string) => Object.keys(definitions[key as 'cennzx'].rpc || {}).length !== 0)
